bdb78600dec814dacacd1386abbf8c22926c803c
it_expert_vikulov <a.vikulov@itexpert.ru>
2019-04-30 16:34:35 +0300

    Added installation packages from requirements file

--- pipInstallLib.cmd
+++ pipInstallLib.cmd
@@ -2,39 +2,4 @@ cd C:\Users\python\Anaconda3\Scripts
 
 python -m pip install --upgrade pip
 
-pip install catboost
-pip install collections
-pip install copy
-pip install gensim
-pip install graphviz
-pip install ipykernel
-pip install Ipython
-pip install IPython display
-pip install itertools
-pip install JSAnimation IPython_display
-pip install keras
-pip install lime
-pip install matplotlib
-pip install mpl_toolkits
-pip install nltk
-pip install numpy
-pip install pandas
-pip install plotly
-pip install random
-pip install re
-pip install seaborn
-pip install sklearn
-pip install sys
-pip install tabulate
-pip install time
-pip install tqdm
-pip install warnings
-pip install xgboost
-pip install codecs
-pip install ipywidgets
-pip install JSAnimation
-pip install mpld3
-pip install scipy
-pip install seaborn 
-pip install statsmodels
-pip install --upgrade tensorflow
+pip install -r requirements.txt
